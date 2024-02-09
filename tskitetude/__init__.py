2d625d7f68d3eb4f565f7cf80e4047fe8b5f5a07
Paolo Cozzi <bunop@libero.it>
2024-02-09 17:02:25 +0100

    :monocle_face: follow tutorial and load data from VCF

--- tskitetude/__init__.py
+++ tskitetude/__init__.py
@@ -4,3 +4,6 @@ import pathlib
 def get_project_dir() -> pathlib.PosixPath:
     mypath = pathlib.Path(__file__)
     return mypath.parents[1]
+
+def get_data_dir() -> pathlib.PosixPath:
+    return get_project_dir() / "data"
