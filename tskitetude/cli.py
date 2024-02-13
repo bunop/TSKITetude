209942ae2d331f13f29b087006ee3cd6366099ff
Paolo Cozzi <bunop@libero.it>
2024-02-13 12:40:41 +0100

    :sparkles: start work on estsfs module

--- tskitetude/cli.py
+++ tskitetude/cli.py
@@ -1,6 +1,50 @@
 
+from typing import List
+
 import click
+from plinkio import plinkfile
+
+from .helper import open_csv
+from .estsfs import search_samples
+
 
 @click.command()
-def make_est_sfs_input():
-    print("Hello world!")
+@click.option(
+    "--plink",
+    help="plink prefix file",
+    type=str,
+    required=True
+)
+@click.option(
+    "--focal",
+    help="focal samples CSV file",
+    type=click.Path(exists=True),
+    required=True
+)
+@click.option(
+    "--outgroup",
+    "outgroups",
+    help="outgroup samples CSV file",
+    multiple=True,
+    type=click.Path(exists=True),
+    required=True,
+)
+def make_est_sfs_input(
+        plink: str, focal: click.Path, outgroups: List[click.Path]):
+
+    # open plink file
+    plink = plinkfile.PlinkFile(plink)
+
+    # read focal samples
+    focal_samples = list(open_csv(focal))
+
+    outgroup_samples = {}
+    outgroup_samples_idx = {}
+
+    # deal with outgroup samples
+    for idx, outgroup in enumerate(outgroups):
+        outgroup_samples[idx] = list(open_csv(outgroup))
+        outgroup_samples_idx[idx] = search_samples(
+            outgroup_samples[idx], plink)
+
+    print(outgroup_samples_idx)
---
+++ tskitetude/estsfs.py
@@ -0,0 +1,18 @@
+
+from plinkio import plinkfile
+from cyvcf2 import VCF
+
+
+def search_samples(samples: list[tuple], plink: plinkfile.PlinkFile):
+    """
+    Search for samples in a plink file a dictionary of sample ids and their
+    index in the plink file.
+    """
+    sample_ids = [sample[1] for sample in samples]
+    samples_idx = {}
+
+    for idx, sample in enumerate(plink.get_samples()):
+        if sample.iid in sample_ids:
+            samples_idx[idx] = sample.iid
+
+    return samples_idx
