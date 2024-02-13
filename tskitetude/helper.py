1e775263406f42bb6e9e0204739b4da18c488901
Paolo Cozzi <bunop@libero.it>
2024-02-13 12:40:09 +0100

    :recycle: rewrite helper module

--- tskitetude/helper.py
+++ tskitetude/helper.py
@@ -3,9 +3,9 @@ import csv
 import tsinfer
 
 
-def add_populations(csv_file: str, samples: tsinfer.SampleData) -> dict:
+def open_csv(csv_file: str) -> csv.reader:
     """
-    Attempt to define metadata like tsinfer tutorial
+    Open a csv file and return a csv.reader object
     """
 
     with open(csv_file) as handle:
@@ -14,13 +14,22 @@ def add_populations(csv_file: str, samples: tsinfer.SampleData) -> dict:
         handle.seek(0)
         reader = csv.reader(handle, dialect)
 
-        # add population to SampleData object and track reference
-        pop_lookup = {}
+        for line in reader:
+            yield line
 
-        for breed, _ in reader:
-            if breed not in pop_lookup:
-                pop_lookup[breed] = samples.add_population(
-                    metadata = {"breed": breed})
+
+def add_populations(csv_file: str, samples: tsinfer.SampleData) -> dict:
+    """
+    Attempt to define metadata like tsinfer tutorial
+    """
+
+    # add population to SampleData object and track reference
+    pop_lookup = {}
+
+    for breed, _ in open_csv(csv_file):
+        if breed not in pop_lookup:
+            pop_lookup[breed] = samples.add_population(
+                metadata = {"breed": breed})
 
     return pop_lookup
 
@@ -31,21 +40,15 @@ def add_diploid_individuals(
     Try to add diploid samples
     """
 
-    with open(csv_file) as handle:
-        sniffer = csv.Sniffer()
-        dialect = sniffer.sniff(handle.read(1024))
-        handle.seek(0)
-        reader = csv.reader(handle, dialect)
+    # track added individuals
+    indv_lookup = {}
 
-        # track added individuals
-        indv_lookup = {}
-
-        for breed, sample_id in reader:
-            population = pop_lookup[breed]
-            indv_lookup[sample_id] = samples.add_individual(
-                ploidy = 2,
-                metadata={"sample_id": sample_id},
-                population=population
-            )
+    for breed, sample_id in open_csv(csv_file):
+        population = pop_lookup[breed]
+        indv_lookup[sample_id] = samples.add_individual(
+            ploidy = 2,
+            metadata={"sample_id": sample_id},
+            population=population
+        )
 
     return indv_lookup
