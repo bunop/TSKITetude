ea064112646263bb5de2299f920f04ba0474230e
Paolo Cozzi <bunop@libero.it>
2024-07-02 14:39:41 +0200

    :monocle_face: subset only one breed for 50K and HD

--- tskitetude/smarterapi.py
+++ tskitetude/smarterapi.py
@@ -11,7 +11,9 @@ class SheepEndpoint():
 
     def get_samples(
             self,
-            type: str = None,
+            _type: str = None,
+            breed: str = None,
+            code: str = None,
             page: int = None,
             size: int = 100) -> dict:
         """Get the samples from the Sheep SMARTER API."""
@@ -21,7 +23,9 @@ class SheepEndpoint():
             params={
                 "size": size,
                 "page": page,
-                "type": type
+                "type": _type,
+                "breed": breed,
+                "breed_code": code
             }
         )
 
@@ -29,3 +33,29 @@ class SheepEndpoint():
             return response.json()
         else:
             raise Exception("Failed to get sheep samples: " + response.text)
+
+
+class BreedEndpoint():
+    url = "https://webserver.ibba.cnr.it/smarter-api/breeds"
+    headers = {}
+
+    def get_breeds(
+            self,
+            species: str = None,
+            page: int = None,
+            size: int = 100) -> dict:
+        """Get the breeds from the Sheep SMARTER API."""
+        response = session.get(
+            self.url,
+            headers=self.headers,
+            params={
+                "size": size,
+                "page": page,
+                "species": species
+            }
+        )
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise Exception("Failed to get sheep breeds: " + response.text)
